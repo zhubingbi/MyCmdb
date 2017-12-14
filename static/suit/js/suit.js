a578c1a7ebeade118d7f61fc50fe5d821466df89
zhubingbi <zhubingbi@gmail.com>
2017-12-14 20:20:37 +0800

    no message

--- static/suit/js/suit.js
+++ static/suit/js/suit.js
@@ -1,13 +1,6 @@
-Suit = {};
-window.Suit = Suit;
-
 (function ($) {
-    if (!$)
-        return;
 
-    Suit.$ = $;
-
-// Register callbacks to perform after inline has been added
+    // Register callbacks to perform after inline has been added
     Suit.after_inline = function () {
         var functions = {};
         var register = function (fn_name, fn_callback) {
@@ -26,91 +19,153 @@ window.Suit = Suit;
         };
     }();
 
-    Suit.ListActionsToggle = function () {
-        var $topActions;
+    // Backwards compatiblity
+    SuitAfterInline = Suit.after_inline;
 
-        var init = function () {
-            $(document).ready(function () {
-                $topActions = $('.results').parent().find('.actions').eq(0);
-                if (!$topActions.length)
-                    return;
-
-                $("tr input.action-select, #action-toggle").on('click', checkIfSelected);
+    /**
+     * Fixed submit buttons.
+     */
+    $.fn.suit_fixed = function () {
+        $(this).each(function () {
+            // extra_offset: 70 (60 Footer height + 10 top offset)
+            var $fixed_item = $(this), pos = $fixed_item.offset(), extra_offset = 70;
+            $(window).bind('scroll.sl resize.sl load.sl', function (e) {
+                var $win = $(this), scroll_top = $win.scrollTop();
+                if ($fixed_item.height() < $win.height() &&
+                    scroll_top > (pos.top - 10) &&
+                    $fixed_item.height() < $win.height()) {
+                    !$fixed_item.hasClass('fixed') && $fixed_item.addClass('fixed');
+                    var max_top = Math.min(10, $(document).height() - $fixed_item.height() - scroll_top - extra_offset);
+                    $fixed_item.css('top', max_top + 'px');
+                }
+                else if (scroll_top <= (pos.top - 10) &&
+                    $fixed_item.hasClass('fixed')) {
+                    $fixed_item.removeClass('fixed');
+                }
             });
+
+            $(window).trigger('scroll.sl');
+        });
+    };
+
+
+    /**
+     * Search filters - submit only changed fields
+     */
+    $.fn.suit_search_filters = function () {
+        $(this).change(function () {
+            var $field = $(this);
+            var $option = $field.find('option:selected');
+            var select_name = $option.data('name');
+            if (select_name) {
+                $field.attr('name', select_name);
+            } else {
+                $field.removeAttr('name');
+            }
+            // Handle additional values for date filters
+            var additional = $option.data('additional');
+            if (additional) {
+                var hidden_id = $field.data('name') + '_add';
+                var $hidden = $('#' + hidden_id);
+                if (!$hidden.length) {
+                    $hidden = $('<input/>').attr('type', 'hidden').attr('id', hidden_id);
+                    $field.after($hidden);
+                }
+                additional = additional.split('=');
+                $hidden.attr('name', additional[0]).val(additional[1])
+            }
+        });
+        $(this).trigger('change');
+    };
+
+    /**
+     * Linked select - shows link to related item after Select
+     */
+    $.fn.suit_linked_select = function () {
+
+        var get_link_name = function ($select) {
+            var text = $select.find('option:selected').text();
+            return text && $select.val() ? text + '' : '';
         };
 
-        var checkIfSelected = function () {
-            if ($('tr.selected').length) {
-                $topActions.slideDown('fast');
-            } else {
-                $topActions.slideUp('fast');
+        var get_url = function ($add_link, $select) {
+            var value = $select.val();
+            return $add_link.attr('href').split('?')[0] + '../' + value + '/';
+        };
+
+        var add_link = function ($select) {
+            var $add_link = $select.next();
+            if ($add_link.hasClass('add-another')) {
+                var $link = $add_link.next('a');
+                if (!$link.length) {
+                    $link = $('<a/>').addClass('linked-select-link');
+                    $add_link.after($link).after(' &nbsp; ');
+                }
+                $link.text(get_link_name($select));
+                $link.attr('href', get_url($add_link, $select));
             }
         };
 
-        return {
-            init: init
+        $(this).each(function () {
+            add_link($(this));
+        });
+
+        $(document).on('change', this.selector, function () {
+            add_link($(this));
+        });
+    };
+
+    /**
+     * Content tabs
+     */
+    $.fn.suit_form_tabs = function () {
+
+        var $tabs = $(this);
+        var tab_prefix = $tabs.data('tab-prefix');
+        if (!tab_prefix)
+            return;
+
+        var $tab_links = $tabs.find('a');
+
+        function tab_contents($link) {
+            return $('.' + tab_prefix + '-' + $link.attr('href').replace('#', ''));
         }
 
-    }();
-
-
-    Suit.FixedBar = function () {
-        var didScroll = false, $fixedItem, $fixedItemParent, $win, $body,
-            itemOffset,
-            extraOffset = 0,
-            fixed = false;
-
-        function init(selector) {
-            $fixedItem = $(selector || '.submit-row');
-            if (!$fixedItem.length)
-                return;
-
-            $fixedItemParent = $fixedItem.parents('form');
-            itemOffset = $fixedItem.offset();
-            $win = $(window);
-            window.onscroll = onScroll;
-            window.onresize = onScroll;
-            onScroll();
-
-            setInterval(function () {
-                if (didScroll) {
-                    didScroll = false;
-                }
-            }, 200);
-        }
-
-        function onScroll() {
-            didScroll = true;
-
-            var itemHeight = $fixedItem.height(),
-                scrollTop = $win.scrollTop();
-
-            if (scrollTop + $win.height() - itemHeight - extraOffset < itemOffset.top) {
-                if (!fixed) {
-                    $fixedItem.addClass('fixed');
-                    $fixedItemParent.addClass('fixed').css('padding-bottom', itemHeight + 'px');
-                    fixed = true;
-                }
+        function activate_tabs() {
+            // Init tab by error, by url hash or init first tab
+            if (window.location.hash) {
+                var found_error;
+                $tab_links.each(function () {
+                    var $link = $(this);
+                    if (tab_contents($link).find('.error').length != 0) {
+                        $link.addClass('error');
+                        $link.trigger('click');
+                        found_error = true;
+                    }
+                });
+                !found_error && $($tabs).find('a[href=\\' + window.location.hash + ']').click();
             } else {
-                if (fixed) {
-                    $fixedItem.removeClass('fixed');
-                    $fixedItemParent.removeClass('fixed').css('padding-bottom', '');
-                    fixed = false;
-                }
+                $tab_links.first().trigger('click');
             }
         }
 
-        return {
-            init: init
-        };
-    }();
+        $tab_links.click(function () {
+            var $link = $(this);
+            $link.parent().parent().find('.active').removeClass('active');
+            $link.parent().addClass('active');
+            $('.' + tab_prefix).removeClass('show').addClass('hide');
+            tab_contents($link).removeClass('hide').addClass('show')
+        });
+
+        activate_tabs();
+    };
 
     /**
      * Avoids double-submit issues in the change_form.
      */
-    $.fn.suitFormDebounce = function () {
+    $.fn.suit_form_debounce = function () {
         var $form = $(this),
-            $saveButtons = $form.find('.submit-row button, .submit-row input[type=button], .submit-row input[type=submit]'),
+            $saveButtons = $form.find('.submit-row button'),
             submitting = false;
 
         $form.submit(function () {
@@ -128,123 +183,17 @@ window.Suit = Suit;
         });
     };
 
-    /**
-     * Content tabs
-     */
-    $.fn.suitFormTabs = function () {
+    $(function () {
 
-        var $tabs = $(this);
-        var tabPrefix = $tabs.data('tab-prefix');
-        if (!tabPrefix)
-            return;
+        // Fixed submit buttons
+        $('.inner-right-column').suit_fixed();
 
-        var $tabLinks = $tabs.find('a');
+        // Show link to related item after Select
+        $('.linked-select').suit_linked_select();
 
-        function tabContents($link) {
-            return $('.' + tabPrefix + '-' + $link.attr('href').replace('#', ''));
-        }
+        // Handle change list filter null values
+        $('.search-filter').suit_search_filters();
 
-        function activateTabs() {
-            // Init tab by error, by url hash or init first tab
-            if (window.location.hash) {
-                var foundError;
-                $tabLinks.each(function () {
-                    var $link = $(this);
-                    if (tabContents($link).find('.error, .errorlist').length != 0) {
-                        $link.addClass('has-error');
-                        $link.trigger('click');
-                        foundError = true;
-                    }
-                });
-                !foundError && $($tabs).find('a[href=\\' + window.location.hash + ']').click();
-            } else {
-                $tabLinks.first().trigger('click');
-            }
-        }
+    });
 
-        $tabLinks.click(function () {
-            var $link = $(this),
-                showEvent = $.Event('shown.suit.tab', {
-                    relatedTarget: $link,
-                    tab: $link.attr('href').replace('#', '')
-                });
-            $link.parent().parent().find('.active').removeClass('active');
-            $link.addClass('active');
-            $('.' + tabPrefix).removeClass('show').addClass('hidden-xs-up');
-            tabContents($link).removeClass('hidden-xs-up').addClass('show');
-            $link.trigger(showEvent);
-        });
-
-        activateTabs();
-    };
-
-    /* Characters count for CharacterCountTextarea */
-    $.fn.suitCharactersCount = function () {
-        var $elements = $(this);
-
-        if (!$elements.length)
-            return;
-
-        $elements.each(function () {
-            var $el = $(this),
-                $countEl = $('<div class="suit-char-count"></div>');
-            $el.after($countEl);
-            $el.on('keyup', function (e) {
-                updateCount($(e.currentTarget));
-            });
-            updateCount($el);
-        });
-
-        function updateCount($el) {
-            var maxCount = $el.data('suit-maxcount'),
-                twitterCount = $el.data('suit-twitter-count'),
-                value = $el.val(),
-                len = twitterCount ? getTweetLength(value) : value.length,
-                count = maxCount ? maxCount - len : len;
-            if (count < 0)
-                count = '<span class="text-danger">' + count + '</span>';
-
-            $el.next().first().html(count);
-        }
-
-        function getTweetLength(input) {
-            var tmp = "";
-            for (var i = 0; i < 23; i++) {
-                tmp += "o"
-            }
-            return input.replace(/(http:\/\/[\S]*)/g, tmp).length;
-        }
-    };
-
-    /**
-     * Search filters - submit only changed fields
-     */
-    $.fn.suitSearchFilters = function () {
-        $(this).change(function () {
-            var $field = $(this);
-            var $option = $field.find('option:selected');
-            var select_name = $option.data('name');
-            if (select_name) {
-                $field.attr('name', select_name);
-            } else {
-                $field.removeAttr('name');
-            }
-            // Handle additional values for date filters
-            var additional = $option.data('additional');
-            console.log($field, additional)
-            if (additional) {
-                var hiddenId = $field.data('name') + '_add';
-                var $hidden = $('#' + hiddenId);
-                if (!$hidden.length) {
-                    $hidden = $('<input/>').attr('type', 'hidden').attr('id', hiddenId);
-                    $field.after($hidden);
-                }
-                additional = additional.split('=');
-                $hidden.attr('name', additional[0]).val(additional[1])
-            }
-        });
-        $(this).trigger('change');
-    };
-
-
-})(typeof django !== 'undefined' ? django.jQuery : undefined);
+}(Suit.$));
